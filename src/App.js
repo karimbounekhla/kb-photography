ba497dfa549d2de2a1f366e179fd01c735320075
karimbounekhla <karim.bounekhla@aimsio.com>
2020-09-20 16:54:35 -0600

    Added basic react routing logic

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
+import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
