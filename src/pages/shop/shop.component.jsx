481029c7314e5939aea346ac0247eb20448a1ace
Karim Bounekhla <karim.bounekhla@mail.utoronto.ca>
2020-11-25 18:59:58 -0700

    Added styles for the note on the shop page

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,9 +2,14 @@ import React from "react";
 
 import CollectionOverview from "../../components/collections-overview/collections-overview.component";
 
+import "./shop.styles.scss";
+
 const ShopPage = () => (
   <div className="shop-page">
-    <span>Note: Shop Content is a work in Progress.</span>
+    <span class="info">
+      <strong>Note:</strong> Shop Content is a work in Progress. The below items
+      are placeholders only.
+    </span>
     <CollectionOverview />
   </div>
 );
